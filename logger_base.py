db97cfcaee60cbeda4fe3b7d84afceb32fcbc332
GermanPublyland <german.estrade@hotmail.com>
2022-03-28 12:21:43 +0200

    Initial commit

---
+++ logger_base.py
@@ -0,0 +1,16 @@
+import logging as log  # https://docs.python.org/3/howto/logging.html
+
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('capa_datos.log', encoding='utf-8'),
+                    log.StreamHandler()
+                ])
+
+if __name__ == '__main__':
+    log.debug('Mensaje a nivel Debug')
+    log.info('Mensaje a nivel de info')
+    log.warning('Mensaje a nivel de warning')
+    log.error('Mensaje a nivel de error')
+    log.critical('Mensaje a nivel de crítico')
